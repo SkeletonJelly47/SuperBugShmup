603012328f75ee82b63a4417dec72aeb5fecd6f9
Jussi Mikkonen <issuj.mik@gmail.com>
2017-10-30 18:41:02 +0200

    Added TestScoreEnemy.prefab to test scores Added OnTriggerAddScore.cs to test scores Added LeaderboardTesting.cs Added PrototypeLeaderboard Scene Added Scene change and score saving to GameLogic.cs

--- BugShmup/Assets/Scripts/GameLogic.cs
+++ BugShmup/Assets/Scripts/GameLogic.cs
@@ -1,12 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameLogic : MonoBehaviour
 {
     public GameObject MasterObject;
+    public int score;
     private void Start()
     {
+        score = 0;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            SaveCurrentScore(score);
+            SceneManager.LoadScene("PrototypeLeaderboard");
+        }
 
+    }
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+    }
+    private void SaveCurrentScore(int value)
+    {
+        //Save current score to PlayerPrefs so it can be used in another scene
+        PlayerPrefs.SetInt("currentScore", value);
     }
 }
---
+++ BugShmup/Assets/Scripts/OnTriggerAddScore.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTriggerAddScore : MonoBehaviour
+{
+    public int score;
+    private GameLogic gameLogic;
+    // Use this for initialization
+    void Start()
+    {
+        gameLogic = GameObject.Find("GameLogic").GetComponent<GameLogic>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Projectile"))
+        {
+            gameLogic.AddScore(score);
+        }
+    }
+}
