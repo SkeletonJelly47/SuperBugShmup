eff1f407dd1afa1fac9dc31474819f7e9c375198
Mikko Peltola <mikkelsson4@gmail.com>
2017-11-03 19:37:28 +0200

    Render texture now created in script, set canvas to scale with screen size, matching method is undecided yet.

---
+++ BugShmup/Assets/Scripts/UI/RenderTextureHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RenderTextureHandler : MonoBehaviour
+{
+    RawImage img;
+    RenderTexture tex;
+    RectTransform rect;
+
+    public int x, y;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Get RawImage and RectTransform
+        img = GetComponent<RawImage>();
+        rect = GetComponent<RectTransform>();
+
+        //Set size to match the texture
+        rect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, x);
+        rect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, y);
+
+        //Create the texture
+        tex = new RenderTexture(x, y, 16, RenderTextureFormat.ARGB32);
+        tex.Create();
+
+        //Assign the target texture and output
+        float cachedCameraAspect = Camera.main.aspect;
+        Camera.main.targetTexture = tex;
+        img.texture = tex;
+
+        //Set camera aspect correctly?!?
+        //Camera.main.aspect = cachedCameraAspect;
+        //Camera.main.rect = new Rect(0, 0, x, y);
+    }
+}
