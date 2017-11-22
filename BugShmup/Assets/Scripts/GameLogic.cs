6f7d9d2b6a82a5654acd035475eff7a27fa7b196
Peltola Mikko TTK17SP <mikkopeltola@kamk.fi>
2017-11-22 18:13:58 +0200

    Began calculations for camera rectanlge size, WIP

--- BugShmup/Assets/Scripts/GameLogic.cs
+++ BugShmup/Assets/Scripts/GameLogic.cs
@@ -8,10 +8,19 @@ public class GameLogic : MonoBehaviour
     //public GameObject Player;
     public static PlayerController player;
     public int score;
-    private void Awake()
+
+    //Boundary variables
+    BoxCollider bulletKill;
+    [SerializeField] GameObject BG;
+    [SerializeField] float boxHeight;
+
+    private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
         score = 0;
+        Debug.Log(Camera.main);
+        CreateBoundaries();
+        //BG = GameObject.FindGameObjectWithTag("BG");
     }
     void Update()
     {
@@ -20,16 +29,27 @@ public class GameLogic : MonoBehaviour
             SaveCurrentScore(score);
             SceneManager.LoadScene("PrototypeLeaderboard");
         }
-
     }
 
     public void AddScore(int newScoreValue)
     {
         score += newScoreValue;
     }
+
     private void SaveCurrentScore(int value)
     {
         //Save current score to PlayerPrefs so it can be used in another scene
         PlayerPrefs.SetInt("currentScore", value);
+    }
+
+    void CreateBoundaries()
+    {
+        bulletKill = new BoxCollider();
+        bulletKill.center = BG.transform.position;
+        float Ydist = Vector3.Distance(BG.transform.position, Camera.main.transform.position);
+        float frustumHeight = 2.0f * Ydist * Mathf.Tan(Camera.main.fieldOfView * 0.5f * Mathf.Deg2Rad);
+        float frustumWidth = frustumHeight * Camera.main.aspect;
+
+        bulletKill.size.Set(frustumWidth, boxHeight, frustumHeight);
     }
 }
