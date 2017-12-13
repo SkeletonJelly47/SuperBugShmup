30f1eec2f916c553f76ad7e928732e2b74016005
Sipi Raussi <sraussi94@gmail.com>
2017-12-13 18:35:20 +0200

    merge

---
+++ BugShmup/Assets/Scripts/Weapons/Player/PlayerProjectileSuperShot.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlayerProjectileSuperShot : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = AudioManager.SFX.SuperShot;
+        audioSource.Play();
+    }
+}
